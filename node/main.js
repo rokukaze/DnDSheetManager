8afb6c8a10aa50c9e5068ec56c7157a437eed724
host server <administrator@host-server>
2017-06-10 19:21:53 -0700

    Base for node server

--- node/main.js
+++ node/main.js
@@ -1,11 +1,45 @@
+var MongoClient = require('mongodb').MongoClient;
+var assert = require('assert');
+var dnd_url = "mongodb://localhost:27017/dnd";
 var express = require('express');
 var app = express();
 
-app.get('/test/:var1/:var2', function(request, response) {
+
+//========== Database functions ==========
+
+var playerQuery = function(response,dbUrl,playerName) {
+
+        MongoClient.connect(dbUrl,function(err,db) {
+
+		if( err == null )
+		{
+			var cursor = db.collection('characters').find({"name":playerName}).toArray( function(err,doc) {
+				assert.equal(err,null);
+				if( doc != null ) { 
+					response.send(doc[0]);
+				}   
+				db.close();
+			}); 
+		}
+		else
+		{
+			response.send("Database error");
+		}
+
+        });
+}
+
+//========== REST functions ==========
+
+app.get('/player/:name', function(request, response) {
+
         response.header("Access-Control-Allow-Origin","*");
-        console.log("param ",request.params.var1);
-	response.send("test");
+	var msg = "received request - stats for player: " + request.params.name;
+	console.log(msg);
+	playerQuery(response,dnd_url,request.params.name);
 })
+
+//==========  Node server ==========
 
 var server = app.listen(18080, function() {
         var host = server.address().address
