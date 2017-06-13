936a8775c13ce2001ae9c7530ef7cf639bc20551
host server <administrator@host-server>
2017-06-13 09:37:28 -0700

    - Updated game query to include info of players in the game

--- node/main.js
+++ node/main.js
@@ -29,16 +29,77 @@ var playerQuery = function(response,dbUrl,playerName) {
         });
 }
 
+var campaignQuery = function(response,dbUrl,campaignName) {
+
+	var playersBack = [];
+	var players;
+
+        MongoClient.connect(dbUrl,function(err,db) {
+
+		if( err == null )
+		{
+			var cursor = db.collection('campaigns').findOne({"name":campaignName},function(err,doc) {
+				console.log(doc);
+				assert.equal(err,null);
+				if( doc != null ) { 
+					playersBack.push(doc);
+					players = doc['players'];
+					db.close();
+					console.log("============");
+					console.log(players);
+					console.log("============");
+					MongoClient.connect(dbUrl,function(err,db) {
+
+						if( err == null )
+						{
+							var playerPull = db.collection('characters').find({}).toArray(function(err,doc) {
+								assert.equal(err,null);
+								if( doc != null ) {
+									playersBack.push(doc);
+									response.send(playersBack);
+								}   
+								db.close();
+							});
+						}
+						else
+						{
+							response.send("Database error");
+							return;
+						}
+					});
+
+
+			}
+			
+		});
+	}		
+	else
+	{
+		response.send("Database error");
+		return;
+	}
+	
+});
+
+}
+
 //========== REST functions ==========
 
 app.get('/player/:name', function(request, response) {
 
-        response.header("Access-Control-Allow-Origin","*");
-	var msg = "received request - stats for player: " + request.params.name;
-	console.log(msg);
-	playerQuery(response,dnd_url,request.params.name);
+response.header("Access-Control-Allow-Origin","*");
+var msg = "received request - stats for player: " + request.params.name;
+console.log(msg);
+playerQuery(response,dnd_url,request.params.name);
 })
 
+app.get('/campaign/:name', function(request, response) {
+
+	response.header("Access-Control-Allow-Origin","*");
+	var msg = "received request - stats for campaign: " + request.params.name;
+	console.log(msg);
+	campaignQuery(response,dnd_url,request.params.name);
+})
 //==========  Node server ==========
 
 var server = app.listen(18080, function() {
