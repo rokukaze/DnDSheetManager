e317ee7e8ef9c0bc4144289724520ddad626fa88
Naz <naseemhammoud@gmail.com>
2017-06-28 14:15:46 -0700

    Externalized frontend javascript to restqueries.js

---
+++ frontend/js/restqueries.js
@@ -0,0 +1,10 @@
+	function getUser() {
+		var xhttp = new XMLHttpRequest();
+		xhttp.open("GET", "http://ec2-34-208-19-115.us-west-2.compute.amazonaws.com:18080/player/rob", false); //find a way to set async to true and have it return when it has something
+		xhttp.setRequestHeader("Content-type", "text/plain");
+		xhttp.send();
+		var response = JSON.stringify(xhttp.responseText);
+		console.log(response);
+		return response;
+	}
+	document.getElementById("user").innerHTML = getUser();
