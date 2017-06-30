c27fe6e86be58ef99ef0f38215a92cb7676b3241
Naz <naseemhammoud@gmail.com>
2017-06-29 22:10:07 -0700

    added Global JSON obj variable and getUser() / getUserText() to easily access the JSON obj

--- frontend/js/restqueries.js
+++ frontend/js/restqueries.js
@@ -1,10 +1,22 @@
-	function getUser() {
-		var xhttp = new XMLHttpRequest();
-		xhttp.open("GET", "http://ec2-34-208-19-115.us-west-2.compute.amazonaws.com:18080/player/rob", false); //find a way to set async to true and have it return when it has something
-		xhttp.setRequestHeader("Content-type", "text/plain");
-		xhttp.send();
-		var response = JSON.stringify(xhttp.responseText);
-		console.log(response);
-		return response;
-	}
-	document.getElementById("user").innerHTML = getUser();
+
+var DnDObj = getUser();
+
+function getUserText() {
+	var xhttp = new XMLHttpRequest();
+	xhttp.open("GET", "http://ec2-34-208-19-115.us-west-2.compute.amazonaws.com:18080/player/rob", false); //find a way to set async to true and have it return when it has something
+	xhttp.setRequestHeader("Content-type", "text/plain");
+	xhttp.send();
+	var response = JSON.stringify(xhttp.responseText);
+	console.log(response);
+	return response;
+}
+
+function getUser() {
+	var xhttp = new XMLHttpRequest();
+	xhttp.open("GET", "http://ec2-34-208-19-115.us-west-2.compute.amazonaws.com:18080/player/rob", false); //find a way to set async to true and have it return when it has something
+	xhttp.setRequestHeader("Content-type", "text/plain");
+	xhttp.send();
+	var response = JSON.parse(xhttp.responseText);
+	console.log(response);
+	return response;
+}
