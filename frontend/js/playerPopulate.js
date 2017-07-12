a0f54b165c119673525de644c4ebd00758dfc7e2
Robert Sanchez <robertsanchez92@gmail.com>
2017-07-11 22:32:07 -0700

    Create user session on successful login

--- frontend/js/playerPopulate.js
+++ frontend/js/playerPopulate.js
@@ -4,9 +4,10 @@ function populatePlayerDetails(playerData) {
 	console.log("Populate player fields");
 	var playerDetailsHTML = generate.rowContent("player-details",playerData["player"]);
 	var playerCharactersHTML = generate.rowContent("player-characters","");
-	var playerAddCharacterHTML = generate.rowContent("player-add-character",generatePlayerAddCharacter(playerData["player"]));
+	var playerAddCharacterHTML = generate.rowContent("player-add-character",generatePlayerAddCharacter({}));
+	var playerLogoutHTML = generate.rowContent("player-logout",generatePlayerLogout());
 
-	var playerInfoHTML = generate.colContent("player-info",2,playerDetailsHTML+playerCharactersHTML+playerAddCharacterHTML);
+	var playerInfoHTML = generate.colContent("player-info",2,playerDetailsHTML+playerCharactersHTML+playerAddCharacterHTML+playerLogoutHTML);
 
 	var playerCharacterHTML = generate.rowContent("character-display","");
 	var playerCharacterDisplayHTML = generate.colContent("player-character-displayer",10,playerCharacterHTML);
@@ -46,11 +47,11 @@ function generatePlayerCharacterList(characters) {
 	return html;
 }
 
-function generatePlayerAddCharacter(playerName) {
+function generatePlayerAddCharacter() {
 
 	var html = "";
 
-	var onclick = "dndPlayer.populateCharacterAdd({'player':'"+playerName+"'})";
+	var onclick = "dndPlayer.populateCharacterAdd({'player':dndSession.currentPlayer()})";
 
 	html += generate.rowContent(null,generate.colWellWithOnClick(12,"player-add-character-display","Create new character","",onclick));
 
@@ -70,6 +71,19 @@ function populateCharacterAdd(characterData) {
 
 	console.log("Populating add character fields");
 	document.getElementById("character-display").innerHTML = generateCharacterInfo(characterData,true);
+}
+
+function generatePlayerLogout() {
+
+	var html = "";
+
+	var onclick = "dndSession.deleteSession();dndLogin.populateLoginPage()"
+
+	html += generate.rowContent(null,generate.colWellWithOnClick(12,"player-logout-display","Logout","",onclick));
+
+	html = generate.colContent(null,8,html);
+
+	return html;
 }
 
 function generateCharacterInfo(characterData,addValue) {
